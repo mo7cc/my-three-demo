4924528b32e316a99ffa755960d64682a8f57b9a
刘龙岗 <llg574231043@163.com>
2024-09-11 10:49:45 +0800

    flx：init

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
+import { Billboard, Float, OrbitControls, Splat, Text } from "@react-three/drei";
 import React, { Suspense } from "react";
+
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Splat } from "@react-three/drei";
 
 function ModelPly() {
+
+  return (
+    <mesh scale={2}
+      >
+      <Splat src="/总场景.splat" />
+    </mesh>
+  );
+}
+function ModelPly2() {
   return (
     <mesh>
-      <Splat src="/test.splat" />
+      <Float>
+        <Splat src="/物体1.splat" scale={0.5} position={[0, 1.6, 2]} />
+      </Float>
     </mesh>
   );
 }
 
+
 export default function App() {
   return (
     <Suspense fallback={"loading..."}>
-      <Canvas>
-        <directionalLight intensity={0.5} />
-        <ambientLight intensity={0.2} />
+      <Canvas shadows  camera={{ position: [20, 20, 20], fov: 35 }} scene>
+        <ambientLight intensity={0.5} />
+        <spotLight position={[50, 10, 50]} angle={0.15} penumbra={1} shadow-mapSize={2048} castShadow />
+        <ModelPly2 />
         <ModelPly />
         <OrbitControls></OrbitControls>
       </Canvas>
